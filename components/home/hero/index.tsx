27010e6a67265d1a79b7a1f61f17347146696930
Reiyna <121340919+Quliyeva1o@users.noreply.github.com>
2026-03-24 14:00:53 -0700

    hero

--- components/home/hero/index.tsx
+++ components/home/hero/index.tsx
@@ -1,6 +1,7 @@
 "use client";
 import Image from "next/image";
 import Button from "../../common/Button";
+import { useEffect, useRef } from "react";
 
 interface HeroProps {
   text?: string;
@@ -11,28 +12,46 @@ interface HeroProps {
 }
 
 export default function Hero({ text, video, isHome, img, subTitle }: HeroProps) {
+  const videoRef = useRef<HTMLVideoElement>(null);
+  useEffect(() => {
+    const video = videoRef.current;
+
+    if (!video) return;
+
+    const playVideo = () => {
+      video.play().catch(() => { });
+    };
+
+    playVideo();
+
+    document.addEventListener("touchstart", playVideo, { once: true });
+
+    return () => {
+      document.removeEventListener("touchstart", playVideo);
+    };
+  }, []);
   const homeTitle = <>Premium hovuz və spa <br /> layihələrinin tikintisi</>;
   const homeText =
     "Hovuzlar, türk hamamı, sauna və spa məkanlarının layihələndirilməsi və tikintisi sahəsində peşəkar xidmət təqdim edirik. Müasir texnologiya, keyfiyyətli materiallar və təcrübəli komandamız ilə evinizdə və obyektinizdə komfortlu istirahət zonaları yaradırıq.";
 
   return (
-    <section className={`${isHome ? 'h-[calc(90vh-150px)]' : 'h-[250px]'}`}>
+    <section className={`${isHome ? 'h-[calc(90vh-180px)] md:h-[calc(90vh-150px)]' : 'md:h-[250px]'}`}>
       <div className={`flex items-center overflow-hidden text-white h-[calc(100%-100px)]`} id="content">
         {video && (
-          <video className="hero-video" autoPlay muted loop playsInline aria-hidden="true">
+          <video ref={videoRef} className="hero-video" autoPlay muted loop playsInline aria-hidden="true">
             <source src={video} type="video/mp4" />
           </video>
         )}
         {
           img &&
           <Image
-           
+
             quality={100}
             src={img}
             alt="Hero Image"
-          fill
-          className="object-cover brightness-75 hero-image"
-           />
+            fill
+            className="object-cover brightness-75 hero-image"
+          />
         }
         <div className="absolute inset-0 bg-black/30" />
 
@@ -42,16 +61,16 @@ export default function Hero({ text, video, isHome, img, subTitle }: HeroProps) 
             Turan İnşaat
           </h3>
 
-          <h1 className="text-[32px] sm:text-[48px] md:text-[56px] lg:text-[60px] font-extrabold leading-[1.1em] tracking-[-0.03em] mb-5 sm:mb-6 font-manrope">
+          <h1 className="text-[24px] sm:text-[36px] md:text-[48px] lg:text-[60px] font-extrabold leading-[1.1em] tracking-[-0.03em] mb-5 sm:mb-6 font-manrope">
             {isHome ? homeTitle : text}
           </h1>
 
-         
-            <p className="text-[16px] sm:text-[18px] max-w-3xl mx-auto leading-[1.5] sm:leading-[1.6] text-[#ddd] mb-8 sm:mb-10 font-dm-sans">
-              {isHome? homeText : subTitle}
-            </p>
-          
-            
+
+          <p className="text-[14px] sm:text-[16px] md:text-[18px] max-w-3xl mx-auto leading-[1.5] sm:leading-[1.6] text-[#ddd] mb-8 sm:mb-10 font-dm-sans">
+            {isHome ? homeText : subTitle}
+          </p>
+
+
 
           {isHome && (
             <div className="flex justify-center gap-4 sm:gap-5 flex-wrap">
